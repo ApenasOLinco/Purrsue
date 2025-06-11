7d79069d4c84e38e84e6004f231895cdcfd38d29
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-11 18:46:57 -0300

    Verificação de campos em branco do formulário

--- auth/login.php
+++ auth/login.php
@@ -3,7 +3,22 @@ $ROOT = $_SERVER['DOCUMENT_ROOT'];
 
 require_once "$ROOT/err/status.php";
 
-if($_SERVER['REQUEST_METHOD'] !== "POST") {
-    $code = Status::METODO_INVALIDO->value;
-    header("location:/pages/index.php?code=$code");
+/**
+ * Manda de volta pra home
+ */
+function bicuda($codigo)
+{
+    header("location:/pages/index.php?code=$codigo");
+}
+
+// Acesso indevido à página
+if ($_SERVER['REQUEST_METHOD'] !== "POST") {
+    $codigo = Status::METODO_INVALIDO->value;
+    bicuda($codigo);
+}
+
+// Formulário em branco
+if (in_array(null, [$_POST['usuario'], $_POST['senha']])) {
+    $codigo = Status::FORM_EM_BRANCO->value;
+    bicuda($codigo);
 }
