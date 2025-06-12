da1a143d703acabdf96dffd43e8eb455aa04121b
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-12 18:16:51 -0300

    Erro adicionado: Não autorizado

--- err/status.php
+++ err/status.php
@@ -6,6 +6,11 @@
 enum Status: int
 {
     /**
+     * Tentativa de acesso a uma parte do site sem uma sessão ativa.
+     */
+    case NAO_AUTORIZADO = 0;
+    
+    /**
      * Nome de usuário ou senha não coincidem com nenhum registro no banco de dados.
      */
     case CREDENCIAIS_INVALIDAS = 1;
@@ -36,7 +41,7 @@ enum Status: int
     case EMAIL_EXISTENTE = 6;
 
     /**
-     * Erro não previsto durante a criação de uma conta
+     * Erro inesperado durante a criação de uma conta
      */
     case ERRO_NA_INSERCAO = 7;
 
@@ -44,22 +49,23 @@ enum Status: int
      * Cadastro realizado com sucesso
      */
     case CADASTRO_SUCESSO = 8;
-
+    
     /**
-     * Retorna a mensagem associada ao objeto que está chamando essa função, de acordo com seu tipo.
+     * Retorna a mensagem associada ao objeto que está chamando essa função.
      * @return string A mensagem associada ao objeto que está chamando essa função
      */
     public function getMensagem(): string
     {
         return match ($this) {
+            self::NAO_AUTORIZADO        => "Você não tem acesso suficiente para ver essa parte do site. Entre em sua conta e tente novamente.",
             self::CREDENCIAIS_INVALIDAS => "Credenciais inválidas. Entre com uma conta existente.",
-            self::METODO_INVALIDO => "Método Inválido. Esse recurso não pode ser acessado dessa forma.",
-            self::FORM_EM_BRANCO => "Formulário em Branco. Preencha todos os campos do formulário.",
-            self::ERRO_NA_CONSULTA => "Ocorreu um erro ao verificar seus dados. Tente novamente em alguns minutos.",
-            self::USUARIO_EXISTENTE => "Esse nome de usuário já está em uso. Tente novamente com outro nome de usuário.",
-            self::EMAIL_EXISTENTE => "Esse endereço de e-mail já está em uso. Tente novamente com outro endereço de e-mail.",
-            self::ERRO_NA_INSERCAO => "Ocorreu um erro ao criar sua conta. Tente novamente em alguns minutos.",
-            self::CADASTRO_SUCESSO => "Cadastro realizado com sucesso. Você já pode fazer login!"
+            self::METODO_INVALIDO       => "Método Inválido. Esse recurso não pode ser acessado dessa forma.",
+            self::FORM_EM_BRANCO        => "Formulário em Branco. Preencha todos os campos do formulário.",
+            self::ERRO_NA_CONSULTA      => "Ocorreu um erro ao verificar seus dados. Tente novamente em alguns minutos.",
+            self::USUARIO_EXISTENTE     => "Esse nome de usuário já está em uso. Tente novamente com outro nome de usuário.",
+            self::EMAIL_EXISTENTE       => "Esse endereço de e-mail já está em uso. Tente novamente com outro endereço de e-mail.",
+            self::ERRO_NA_INSERCAO      => "Ocorreu um erro ao criar sua conta. Tente novamente em alguns minutos.",
+            self::CADASTRO_SUCESSO      => "Cadastro realizado com sucesso. Você já pode fazer login!"
         };
     }
 }
