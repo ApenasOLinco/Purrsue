20d37d2d6f5d3cd18f9260020b832cec7309b5bb
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-11 22:21:19 -0300

    Processo de login adicionado

--- auth/login.php
+++ auth/login.php
@@ -6,29 +6,73 @@ require_once "$ROOT/err/status.php";
 /**
  * Te chuta de volta pra home. Ai! Ai!
  */
-function bicuda($codigo)
+function bicuda(Status $codigo)
 {
-    header("location:/pages/index.php?code=$codigo");
-    die();  
-    //   ^
-    // /____\   
-    // | []n|  Nesta casa, só usamos die()           
+    header("location:/pages/index.php?codigo=$codigo->value");
+    die; // É tão forte que o código morre
 }
 
 // Acesso indevido à página
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    $codigo = Status::METODO_INVALIDO->value;
+    $codigo = Status::METODO_INVALIDO;
     bicuda($codigo);
 }
 
 // Formulário em branco
 if (in_array(null, [$_POST['usuario'], $_POST['senha']])) {
-    $codigo = Status::FORM_EM_BRANCO->value;
+    $codigo = Status::FORM_EM_BRANCO;
     bicuda($codigo);
 }
 
 // Obtém os dados do corpo da requisição
 [$usuario, $senha] = [$_POST['usuario'], $_POST['senha']];
 
+// Preparação da consulta
 require_once "$ROOT/connection/conn.php";
 
+$conn = conectar_bd();
+$query = 'SELECT * FROM _usuarios WHERE usuario = ? AND senha = ?';
+$stmt = mysqli_prepare($conn, $query);
+
+// Erro ao preparar a consulta
+if (!$stmt) {
+    mysqli_close($conn);
+    $codigo = Status::ERRO_NA_CONSULTA;
+    bicuda($codigo);
+}
+
+mysqli_stmt_bind_param($stmt, 'ss', $usuario, $senha);
+
+// Execução da consulta
+if (!mysqli_execute($stmt)) {
+    mysqli_close($conn);
+    $codigo = Status::ERRO_NA_CONSULTA;
+    bicuda($codigo);
+}
+
+// Consultar quantas colunas existem no resultado
+mysqli_stmt_store_result($stmt);
+$linhas = mysqli_stmt_num_rows($stmt);
+
+// Usuário ou senha incorretos
+if ($linhas <= 0) {
+    $codigo = Status::CREDENCIAIS_INVALIDAS;
+    bicuda($codigo);
+}
+
+// Obter os resultados
+mysqli_stmt_bind_result($stmt, $id, $usuario, $senha, $email);
+mysqli_stmt_fetch($stmt);
+
+// Fechar a conexão
+mysqli_close($conn);
+
+// O usuário entrou!!! Yay!!!
+session_start();
+$_SESSION['id'] = $id;
+$_SESSION['usuario'] = $usuario;
+$_SESSION['senha'] = $senha;
+$_SESSION['email'] = $email;
+
+// Redirecionar pra Home
+header('location:/pages/home.php');
--- err/status.php
+++ err/status.php
@@ -11,12 +11,15 @@ enum Status: int
 
     case FORM_EM_BRANCO = 3;
 
+    case ERRO_NA_CONSULTA = 4;
+    
     public function getMensagem(): string
     {
         return match ($this) {
             self::CREDENCIAIS_INVALIDAS => "Credenciais inválidas. Entre com uma conta existente.",
             self::METODO_INVALIDO => "Método Inválido. Esse recurso não pode ser acessado dessa forma.",
-            self::FORM_EM_BRANCO => "Formulário em Branco. Preencha todos os campos do formulário."
+            self::FORM_EM_BRANCO => "Formulário em Branco. Preencha todos os campos do formulário.",
+            self::ERRO_NA_CONSULTA => "Ocorreu um erro ao verificar seus dados. Tente novamente em alguns minutos."
         };
     }
 }
