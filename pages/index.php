939c64079e5eb5399562949305e4bca94e527b25
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-11 22:22:09 -0300

    Form de cadastro adicionado

--- pages/index.php
+++ pages/index.php
@@ -15,29 +15,39 @@ require_once "$ROOT/err/status.php";
 
 <body>
    <?php
-   // Cabeçalho das páginas
+   // Cabeçalho (Nav) da página
    require_once "$ROOT/components/header.html";
 
-   // Tratamento dos códigos enviados via GET
-   $code = Status::tryFrom($_GET['code']);
-   if (isset($code)) {
-      echo $code->getMensagem();
+   // Tratamento dos códigos de erro enviados via GET
+   $codigo = Status::tryFrom($_GET['codigo']);
+   if (isset($codigo)) {
+      echo $codigo->getMensagem();
    }
    ?>
 
-   <form action="/auth/login.php" method="post">
-      <label for="usuario">Nome de Usuário</label>
-      <input type="text" name="usuario" id="usuario-input">
+   <!-- Formulário de Login -->
+   <form action="/auth/login.php" method="post" id="login-form">
+      <label for="usuario">Nome de Usuário:</label>
+      <input type="text" name="usuario" id="login-usuario-input">
 
       <label for="senha">Senha:</label>
-      <input type="password" name="senha" id="senha-input">
+      <input type="password" name="senha" id="login-senha-input">
 
-      <input type="submit" value="Entrar">
+      <input type="submit" value="Entrar!">
    </form>
 
    <h3 class="nao-tem-conta-call">Ainda não tem conta?? Crie uma!</h3>
+   <form action="/auth/cadastro.php" method="post" id="cadastro-form">
+      <label for="usuario">Nome de Usuário:</label>
+      <input type="text" name="usuario" id="cadastro-usuario-input">
 
-   <form action="/auth/cadastro.php">
+      <label for="senha">Senha:</label>
+      <input type="password" name="senha" id="cadastro-senha-input">
+
+      <label for="cadastro-email-input">E-Mail:</label>
+      <input type="email" name="email" id="cadastro-email-input">
+
+      <input type="submit" value="Criar conta!">
    </form>
 </body>
 
