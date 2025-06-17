18df8309b5b1bbe5ae3304662e333dc44a734669
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-16 22:12:07 -0300

    Adicionada lógica de popover ao clicar para ver as fotinhos de um gato

---
+++ pages/Home/home.js
@@ -0,0 +1,9 @@
+const gatoInfos = document.querySelectorAll('.gato-info-row');
+
+if (gatoInfos.length > 0) {
+    gatoInfos.forEach(row => {
+        row.addEventListener('click', () => {
+            
+        });
+    });
+}
--- pages/Home/home.php
+++ pages/Home/home.php
@@ -30,20 +30,57 @@ require_once "$ROOT/auth/cadeado.php";
     $conn = conectar_bd();
 
     $usuario_id = $_SESSION['id'];
-    $query = "SELECT * FROM _gatos WHERE usuario_id = $usuario_id";
-    $resultado = mysqli_query($conn, $query);
 
-    if (!$resultado) {
+    $query =
+        "SELECT
+            g.*,
+            fg.gato_foto_caminho
+        FROM _gatos g
+        INNER JOIN _fotos_gatos fg ON g.id = fg.gato_id
+        WHERE g.usuario_id = $usuario_id";
+
+    $result = mysqli_query($conn, $query);
+
+    $gatos = [];
+
+    // Armazenar os gatos em um array associativo
+    while ($row = mysqli_fetch_assoc($result)) {
+        // id do gato atual
+        $gato_id = $row['id'];
+
+        // Se o gato ainda não tiver sido armazenado, armazenar ele
+        if (!isset($gatos[$gato_id])) {
+            $gatos[$gato_id] = [
+                'id' => $gato_id,
+                'nome' => $row['nome'],
+                'descricao' => $row['descricao'],
+                'raca' => $row['raca'],
+                'fotos' => []
+            ];
+        }
+
+        // Adiciona a foto do resultado atual no gato com o id do resultado atual
+        $gatos[$gato_id]['fotos'][] = $row['gato_foto_caminho'];
+    }
+
+
+    // Erro ao recuperar informaçõe do banco de dados
+    if (!$result) {
         $codigo = Status::ERRO_NA_CONSULTA;
         bicuda($codigo);
     }
 
     // Nenhum gato cadastrado :(
-    if (mysqli_num_rows($resultado) < 1): ?>
+    if (mysqli_num_rows($result) < 1): ?>
         <h3 id="como-assim-h3">Quê?? Você ainda não cadastrou NENHUM GATO? D:</h3>
-    <?php die(); endif;
+        <?php die();
+    endif;
+
+    // Fechar a conexão
+    mysqli_close($conn);
 
     ?>
+
     <!-- Estrutura da tabela -->
     <table>
         <tr>
@@ -57,23 +94,29 @@ require_once "$ROOT/auth/cadeado.php";
 
         <tbody>
             <?php // Gatos
-            while ($gato = mysqli_fetch_assoc($resultado)): ?>
-                <tr>
+            foreach ($gatos as $gato): ?>
+                <tr class="gato-info-row">
                     <td><?= $gato['nome'] ?></td>
-
                     <td><?= $gato['raca'] ?></td>
-
                     <td><?= $gato['descricao'] ?></td>
-
                     <td>
-                        <a class="tabela-acao excluir" href="/auth/gatos/excluir.php?<?= $gato['id'] ?>">Excluir</a>
+                        <a class="tabela-acao excluir" href="/auth/gatos/excluir.php?<?= $gato['id'] ?>">
+                            <button>Excluir</button>
+                        </a>
+
+                        <button popovertarget="fotinho<?= $gato['id'] ?>">Ver fotos!!</button>
+
+                        <!-- Popover com fotinhos do gato -->
+                        <div id="fotinho<?= $gato['id'] ?>" class="fotinhos" popover>
+                            <?php foreach ($gato['fotos'] as $foto): ?>
+                                <img src="<?= $foto ?>" alt="fotinho do gato">
+                            <?php endforeach; ?>
+                        </div>
+
                     </td>
                 </tr>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </tbody>
-
-        <?php mysqli_close($conn); ?>
-
     </table>
     <script src="home.js"></script>
 </body>
